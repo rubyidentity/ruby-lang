a56439206e2641cd699a366bdf80071b7d715a60
John W. Long <me@johnwlong.com>
2009-09-01 11:55:23 -0400

    tweaked migrations for old db

--- db/schema.rb
+++ db/schema.rb
@@ -9,21 +9,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 4) do
+ActiveRecord::Schema.define(:version => 20081203140407) do
 
   create_table "config", :force => true do |t|
     t.string "key",   :limit => 40, :default => "", :null => false
     t.string "value",               :default => ""
   end
 
+  create_table "extension_meta", :force => true do |t|
+    t.string  "name"
+    t.integer "schema_version", :default => 0
+    t.boolean "enabled",        :default => true
+  end
+
   create_table "layouts", :force => true do |t|
-    t.string   "name",         :limit => 100
+    t.string   "name",          :limit => 100
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "created_by"
-    t.integer  "updated_by"
-    t.string   "content_type", :limit => 40
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.string   "content_type",  :limit => 40
+    t.integer  "lock_version",                 :default => 0
   end
 
   create_table "page_parts", :force => true do |t|
@@ -33,45 +40,69 @@ ActiveRecord::Schema.define(:version => 4) do
     t.integer "page_id"
   end
 
+  add_index "page_parts", ["page_id", "name"], :name => "parts_by_page"
+
   create_table "pages", :force => true do |t|
     t.string   "title"
-    t.string   "slug",         :limit => 100
-    t.string   "breadcrumb",   :limit => 160
-    t.string   "behavior_id",  :limit => 25
-    t.integer  "status_id",                   :default => 1,     :null => false
+    t.string   "slug",          :limit => 100
+    t.string   "breadcrumb",    :limit => 160
+    t.string   "class_name",    :limit => 25
+    t.integer  "status_id",                    :default => 1,     :null => false
     t.integer  "parent_id"
     t.integer  "layout_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "published_at"
-    t.integer  "created_by"
-    t.integer  "updated_by"
-    t.boolean  "virtual",                     :default => false, :null => false
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.boolean  "virtual",                      :default => false, :null => false
+    t.integer  "lock_version",                 :default => 0
+    t.string   "description"
+    t.string   "keywords"
   end
 
+  add_index "pages", ["class_name"], :name => "pages_class_name"
+  add_index "pages", ["parent_id"], :name => "pages_parent_id"
+  add_index "pages", ["slug", "parent_id"], :name => "pages_child_slug"
+  add_index "pages", ["virtual", "status_id"], :name => "pages_published"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id"
+    t.text     "data"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
   create_table "snippets", :force => true do |t|
-    t.string   "name",       :limit => 100, :default => "", :null => false
-    t.string   "filter_id",  :limit => 25
+    t.string   "name",          :limit => 100, :default => "", :null => false
+    t.string   "filter_id",     :limit => 25
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "created_by"
-    t.integer  "updated_by"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.integer  "lock_version",                 :default => 0
   end
 
   add_index "snippets", ["name"], :name => "name", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "name",       :limit => 100
+    t.string   "name",          :limit => 100
     t.string   "email"
-    t.string   "login",      :limit => 40,  :default => "", :null => false
-    t.string   "password",   :limit => 40
-    t.integer  "admin",                     :default => 0,  :null => false
-    t.integer  "developer",                 :default => 0,  :null => false
+    t.string   "login",         :limit => 40,  :default => "",    :null => false
+    t.string   "password",      :limit => 40
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "created_by"
-    t.integer  "updated_by"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.boolean  "admin",                        :default => false, :null => false
+    t.boolean  "developer",                    :default => false, :null => false
+    t.text     "notes"
+    t.integer  "lock_version",                 :default => 0
+    t.string   "salt"
+    t.string   "session_token"
   end
 
   add_index "users", ["login"], :name => "login", :unique => true
